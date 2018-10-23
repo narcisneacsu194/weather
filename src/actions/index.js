4395f1e0fe8bc0767eed73340f33216a8bcb773b
Narcis Neacsu <narcisneacsu194@gmail.com>
2018-10-23 10:58:00 +0300

    Initial commit.

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const API_KEY = 'd0a0bbe54a4b38ee968cdca2375ab1df';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
